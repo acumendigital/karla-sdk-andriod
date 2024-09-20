6c931c953a0d9adb0e31b38560bd01d5a74c8c16
elvis-chuks <celvischuks@gmail.com>
2024-09-20 22:42:32 +0100

    upgraded gradle

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -66,7 +66,7 @@ configure<PublishingExtension> {
         create<MavenPublication>("Maven") {
             artifactId = "karla-sdk-andriod"
             groupId = "com.github.acumendigital"
-            version = "0.4.0"
+            version = "0.4.1"
             afterEvaluate {
                 artifact(tasks.getByName(
                     "bundleReleaseAar"
